72fb2795be1755415dafff656c6e539f1f6c2311
HiteshRupavatiya <hitesh@zignuts.com>
2023-02-24 15:38:01 +0530

    WelcomeUserNotification added for send custom notification via mail

---
+++ mail_notification_demo/app/Notifications/WelcomeUserNotification.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class WelcomeUserNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    public $format;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($format)
+    {
+        $this->format = $format;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->greeting($this->format['greeting'])
+                    ->line($this->format['body'])
+                    ->line($this->format['thanks']);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
