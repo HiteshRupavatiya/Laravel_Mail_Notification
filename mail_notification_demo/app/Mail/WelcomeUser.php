1d088cd7c3f25f9ffd8ea22a55c388cf59614df2
HiteshRupavatiya <hitesh@zignuts.com>
2023-02-24 15:44:09 +0530

    change the Envelope() method Address and subject, modify content() method and return the welcome_user.blade.php

--- mail_notification_demo/app/Mail/WelcomeUser.php
+++ mail_notification_demo/app/Mail/WelcomeUser.php
@@ -5,6 +5,7 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
@@ -13,14 +14,16 @@ class WelcomeUser extends Mailable
 {
     use Queueable, SerializesModels;
 
+    public $user;
+
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($user)
     {
-        //
+        $this->user = $user;
     }
 
     /**
@@ -31,7 +34,8 @@ class WelcomeUser extends Mailable
     public function envelope()
     {
         return new Envelope(
-            subject: 'Welcome User',
+            from: new Address('hello@example.com',env('MAIL_FROM_NAME')),
+            subject: 'Welcome Thank You For Registration',
         );
     }
 
@@ -43,7 +47,7 @@ class WelcomeUser extends Mailable
     public function content()
     {
         return new Content(
-            view: 'view.name',
+            view: 'email.welcome_user',
         );
     }
 
